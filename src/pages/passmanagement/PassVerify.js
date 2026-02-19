dff837827f1d2f948284586fb28252f6c3110030
mrbi10 <hetmyerwi@gmail.com>
2026-02-19 14:37:56 +0530

    removed outer div

--- src/pages/passmanagement/PassVerify.js
+++ src/pages/passmanagement/PassVerify.js
@@ -69,7 +69,6 @@ export default function PassVerify() {
   const pass = result.pass;
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
 
       <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8 text-center">
 
@@ -127,6 +126,5 @@ export default function PassVerify() {
         </div>
       </div>
 
-    </div>
   );
 }
